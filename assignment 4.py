5646ea40e3af02ddd56f6c91cf821b866f62ca82
Yuvraj1222-stack <21ysaini.acc@lwetb.ie>
2025-09-02 14:14:10 +0100

    Add files via upload

---
+++ assignment 4.py
@@ -0,0 +1,10 @@
+#author:Yuvraj Saini
+# date: 1st september, 2025,monday
+#description: problme 8, days to hours,mins and seconds
+#days,minutes,/60
+amount_days=input('enter random number of days :')
+days=int(amount_days)
+minutes=int(days*60)
+print('amount of minutes in that amount of days are',minutes,'minutes')
+seconds=int(minutes*60)
+print('amount of seconds in that amount of days are', seconds,'seconds')
