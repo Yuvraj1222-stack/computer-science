c58f90684f93d20f4d564ee47d0aa13d2b61ccb0
Yuvraj1222-stack <21ysaini.acc@lwetb.ie>
2025-09-15 00:13:42 +0100

    Update and rename assignment 3 re.py to for loops re:/assignment 3 re.py

--- for loops re:/assignment 3 re.py
+++ for loops re:/assignment 3 re.py
@@ -1,7 +1,6 @@
 # Yuvraj Saini
 #Code for sum of all number from 1 to a give number. 
 
-
 n = int(input("Enter a number: "))
 total = 0
 
@@ -9,3 +8,4 @@ for i in range(1, n + 1):
     total = total + i
 
 print("Sum is:", total)
+
