435bf799fb84bcccc68cfcf82a3f7fe4a806caf2
Yuvraj1222-stack <21ysaini.acc@lwetb.ie>
2025-09-14 22:59:22 +0100

    Add files via upload

--- assignment 1.py
+++ assignment 1.py
@@ -1,24 +1,3 @@
-#Author: Yuvraj
-#Date: 01 Septemeber 2025
-#Description: program to requesr user name as input and return same with a greeting
-
-
-
-
-
-
-
-user_firstname = input('First Name : ')
-user_surname = input('Surname Name : ')
-print('Hello'+ ',',user_firstname,user_surname)
-
-num1 = input('enter first numbers : ')
-num2 = input('enter second numbers : ')
-num3 = input('enter third numbers : ')
-num1=int(num1)
-num2=int(num2)
-num3=int(num3)
-total1=(num1+num2)
-result=(total1*num3)
-print('the answer is',result)
-
+# Printing the first 10 natural numbers
+for i in range(1, 11):  # range goes from 1 to 10
+    print(i)
