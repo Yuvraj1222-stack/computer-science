6a89f370ce92d26ad85649bd2363700db3ceec02
Yuvraj1222-stack <21ysaini.acc@lwetb.ie>
2025-09-21 23:00:13 +0100

    Add files via upload

---
+++ area and perimeter.py
@@ -0,0 +1,8 @@
+length = float(input("Length of rectangle: "))
+width = float(input("Width of rectangle: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area =", area)
+print("Perimeter =", perimeter)
