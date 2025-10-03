d8086dd54803a27ccdf8abaa952c69738b73ae21
Yuvraj1222-stack <21ysaini.acc@lwetb.ie>
2025-10-03 09:45:18 +0100

    Add files via upload

---
+++ Q1 while loops.py
@@ -0,0 +1,19 @@
+# Yuvraj
+# while loops,Q1
+
+n = -1
+total =0
+count = 0
+while n!= '':
+    n = input('enter a number')
+    if n.isdigit():
+        total = total + int(n)
+        count = count + 1
+
+
+
+
+print(total/count)
+        
+
+       
