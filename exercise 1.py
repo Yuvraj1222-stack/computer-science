84559192ffaf7941e954038be2662d9e6983b5f9
Yuvraj1222-stack <21ysaini.acc@lwetb.ie>
2025-10-24 09:37:49 +0100

    Add files via upload

---
+++ exercise 1.py
@@ -0,0 +1,27 @@
+#Yuvraj
+#string assignment
+
+variable = input('Enter your string: ')
+print(variable.upper())
+
+print(variable.lower())
+
+print(variable.count(a))
+
+print(variable.find(p))
+
+print(variable.replace(a,c))
+
+print(variable.islower())
+
+print(variable.isupper())
+
+print(variable.isalnum())
+
+print(variable.isaplha())
+
+print(variable.isdigit())
+
+print(variable.index(s))
+
+print(variable.strip(f))
